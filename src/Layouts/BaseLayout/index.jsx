444ad7f181a1e46f53356cb4a2f06f9cbe0f621c
17302377913 <2647418717@qq.com>
2024-07-16 17:22:39 +0800

    feat: 配置layout主体结构

--- src/Layouts/BaseLayout/index.jsx
+++ src/Layouts/BaseLayout/index.jsx
@@ -1,5 +1,15 @@
+import { Layout } from 'antd'
+import { Content, Header, Footer } from 'antd/es/layout/layout'
 import React from 'react'
 
 export default function BaseLayout() {
-  return <div>BaseLayout</div>
+  return (
+    <div>
+      <Layout>
+        <Header></Header>
+        <Content></Content>
+        <Footer></Footer>
+      </Layout>
+    </div>
+  )
 }
