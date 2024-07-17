03d54513cb944003473b1023fe1ed3b86a42c1ec
17302377913 <2647418717@qq.com>
2024-07-17 10:56:42 +0800

    feat: 导入icon，配置基本layout

--- src/Layouts/BaseLayout/index.jsx
+++ src/Layouts/BaseLayout/index.jsx
@@ -1,15 +1,14 @@
 import { Layout } from 'antd'
 import { Content, Header, Footer } from 'antd/es/layout/layout'
 import React from 'react'
+import style from './index.module.less'
 
 export default function BaseLayout() {
   return (
-    <div>
-      <Layout>
-        <Header></Header>
-        <Content></Content>
-        <Footer></Footer>
-      </Layout>
-    </div>
+    <Layout className={style.layoutContainer}>
+      <Header className={style.layoutHeader}>{/* nav */}1</Header>
+      <Content className={style.layoutContent}>{/* 主体部分 */}2</Content>
+      <Footer className={style.layoutFooter}>3</Footer>
+    </Layout>
   )
 }
