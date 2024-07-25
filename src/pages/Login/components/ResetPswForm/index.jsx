84f9512f57be908862197a63aac7b0207e21930c
17302377913 <2647418717@qq.com>
2024-07-25 17:33:02 +0800

    feact: 添加忘记密码组件

---
+++ src/pages/Login/components/ResetPswForm/index.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import style from './index.module.less'
+import { Flex, Select } from 'antd'
+import { CheckCircleFilled } from '@ant-design/icons'
+
+const tabList = [
+  {
+    id: 1,
+    label: '邮箱'
+  },
+  {
+    id: 2,
+    label: '手机号'
+  }
+]
+const options = [
+  {
+    value: 1,
+    label: (
+      <>
+        <CheckCircleFilled />
+        +852
+      </>
+    )
+  },
+  {
+    value: 2,
+    label: (
+      <>
+        <CheckCircleFilled />
+        +812
+      </>
+    )
+  }
+]
+export default function ResetPswForm() {
+  const [checkTab, setCheckTab] = useState(1)
+  return (
+    <div className={style.resetContainer}>
+      <header>
+        <h3>重置登录密码</h3>
+        <div className={style.tag}>
+          重置登录密码后，24小时内禁止提币和OTC划转
+        </div>
+      </header>
+      <div className={style.tabs}>
+        {tabList.map((item) => (
+          <span
+            key={item.id}
+            className={checkTab === item.id ? style.active : ''}
+            onClick={() => {
+              setCheckTab(item.id)
+            }}
+          >
+            {item.label}
+          </span>
+        ))}
+      </div>
+      {/* 邮箱注册 */}
+      <div
+        className={style.emailForm}
+        style={{ display: checkTab === 1 ? 'block' : 'none' }}
+      >
+        <input placeholder="请输入邮箱" />
+        <button
+          onClick={() => {
+            toggleCurrentStatus(2, 1)
+          }}
+        >
+          下一步
+        </button>
+      </div>
+      {/* 手机号注册 */}
+      <div
+        className={style.telForm}
+        style={{ display: checkTab === 2 ? 'block' : 'none' }}
+      >
+        <Flex>
+          <Select options={options} />
+          <input placeholder="手机号" style={{ width: '264px' }} />
+        </Flex>
+        <button
+          onClick={() => {
+            toggleCurrentStatus(2, 2)
+          }}
+        >
+          下一步
+        </button>
+      </div>
+    </div>
+  )
+}
