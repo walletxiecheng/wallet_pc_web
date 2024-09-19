267d73d2b31b0b5d3228f3cfc96a2de4aa486653
17302377913 <2647418717@qq.com>
2024-09-19 18:05:24 +0800

    fix: 修改语言配置

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,9 +2,11 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from './locales/en.json'
 import zh from './locales/zh.json'
-import { useLocation } from 'react-router-dom'
 
-// console.log(window.location.search)
+console.log(location.search)
+const search = location.search
+const lang = search.split('=')[1]
+console.log(lang)
 const resources = {
   // en:英文;fr:法文;zh:中文
   en: en,
@@ -13,7 +15,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'en',
+  lng: lang || 'zh',
   fallbackLng: 'zh',
   interpolation: {
     escapeValue: false
