c3ae185a35f18a1a693f427fe2171f9cf1dbacf0
17302377913 <2647418717@qq.com>
2024-12-26 17:30:58 +0800

    添加货币计算器中英文切换配置文件

--- src/pages/subPages/Calculator/index.jsx
+++ src/pages/subPages/Calculator/index.jsx
@@ -8,11 +8,13 @@ import iconChangeLine from '@/assets/icon/light/icon-change-line.svg'
 import iconAlertWarnLine from '@/assets/icon/light/icon-alert-line-warn.svg'
 import arrowLeftIcon from '@/assets/icon/light/icon-arrow-left-line.png'
 import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { URLS } from '@/routes/urls'
 
 const { Option } = Select
 export default function Calculator() {
   const navigate = useNavigate()
-
+  const { t } = useTranslation()
   const { data } = useRequest(async () => {
     try {
       const { data } = await getRate()
@@ -22,7 +24,6 @@ export default function Calculator() {
 
   const [usdRate, setUsdRate] = useState(1)
   const [usdTRate, setUsdTRate] = useState(1)
-
   const [inputVal, setInputValue] = useState(0)
 
   const getOutPutVal = () => {
@@ -39,24 +40,20 @@ export default function Calculator() {
         <div
           className={style.back}
           onClick={() => {
-            navigate(-1)
+            navigate(URLS.index)
           }}
         >
           <img src={arrowLeftIcon} width={16} alt="" />
-          返回首页
+          {t('calculator.back')}
         </div>
         <div className={style.box}>
-          <header>数字货币计算器</header>
-          <div className={style.desc}>
-            本页面显示了数字货币与法币的实时汇率。您可以立即将 100
-            多种热门数字货币，如比特币 (BTC)，以太坊 (ETH) 和瑞波 (XRP)
-            转换为您所需的法币。
-          </div>
+          <header>{t('calculator.title')}</header>
+          <div className={style.desc}>{t('calculator.description')}</div>
         </div>
 
         <div className={style.coinBox}>
           <div className={style.left}>
-            <div>我要卖出</div>
+            <div>{t('calculator.sell')}</div>
             <input
               type="text"
               className={style.coinInput}
@@ -67,7 +64,7 @@ export default function Calculator() {
             />
           </div>
           <Select
-            placeholder="选择货币"
+            placeholder={t('calculator.Select currency')}
             onChange={(value) => {
               setUsdTRate(value)
             }}
@@ -84,11 +81,11 @@ export default function Calculator() {
         </div>
         <div className={style.coinBox}>
           <div className={style.left}>
-            <div>我将收到</div>
+            <div>{t('calculator.receive')}</div>
             <div className={style.outInput}>{parseFloat(getOutPutVal())}</div>
           </div>
           <Select
-            placeholder="选择法币"
+            placeholder={t('calculator.Select fiat currency')}
             onChange={(value) => {
               setUsdRate(value)
             }}
@@ -103,9 +100,9 @@ export default function Calculator() {
 
         <div className={style.tips}>
           <div>1 USDT ≈ 1 USD</div>
-          <div className="tipBox">
+          <div className={style.tipBox}>
             <img src={iconAlertWarnLine} style={{ marginRight: 3 }} />
-            提示：由于汇率波动频繁，该结果仅作为参考使用
+            {t('calculator.tip')}
           </div>
         </div>
       </div>
